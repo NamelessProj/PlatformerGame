7bb7bdd2417b5d649756ad4939df3ebdf4c67ff3
Kevin <pintokevin2002@hotmail.com>
2025-06-07 14:27:14 +0200

    Add GameConstants class for centralized game configuration values

---
+++ src/main/java/constants/GameConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class GameConstants {
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 800;
+    public static final int PLAYER_IMAGE_WIDTH = 64;
+    public static final int PLAYER_IMAGE_HEIGHT = 40;
+    public static final int IMAGE_WIDTH = 128;
+    public static final int IMAGE_HEIGHT = 80;
+}
