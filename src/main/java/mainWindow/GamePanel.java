24e78e52e362ac907c1c21dc0f212160c2f44770
Kevin <pintokevin2002@hotmail.com>
2025-06-07 13:41:28 +0200

    Enhance GamePanel to include dynamic rectangle movement and color change

--- src/main/java/mainWindow/GamePanel.java
+++ src/main/java/mainWindow/GamePanel.java
@@ -4,38 +4,66 @@ import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
 import javax.swing.JPanel;
-import java.awt.Graphics;
+import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(190, 20, 50);
+    private Random random;
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
         this.addKeyListener(new KeyboardInputs(this));
         this.addMouseListener(mouseInputs);
         this.addMouseMotionListener(mouseInputs);
+        this.random = new Random();
     }
 
     public void changeXDelta(int val) {
         this.xDelta += val;
-        this.repaint();
     }
 
     public void changeYDelta(int val) {
         this.yDelta += val;
-        this.repaint();
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        this.repaint();
+    }
+
+    private void getRandomColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        color = new Color(r, g, b);
+    }
+
+    private void updateRect() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            getRandomColor(); // Change color when bouncing off the edges
+        }
+
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            getRandomColor(); // Change color when bouncing off the edges
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50); // Example drawing a rectangle
+        updateRect();
+
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50); // Example drawing a rectangle
     }
 }
