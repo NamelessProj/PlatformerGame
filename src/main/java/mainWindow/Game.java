42e2d5715b87adb2782df4efc38e60e8ba6a49bc
Kevin <pintokevin2002@hotmail.com>
2025-06-07 19:08:21 +0200

    Replace local FPS_SET variable with import from GameConstants

--- src/main/java/mainWindow/Game.java
+++ src/main/java/mainWindow/Game.java
@@ -1,10 +1,11 @@
 package mainWindow;
 
+import static utils.Constants.GameConstants.FPS_SET;
+
 public class Game implements Runnable {
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private Thread gameThread;
-    private final int FPS_SET = 120;
 
     public Game() {
         gamePanel = new GamePanel();
