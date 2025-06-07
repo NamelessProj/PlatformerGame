bc87ab94d4da1f27fd9916a9671538c22327e04f
Kevin <pintokevin2002@hotmail.com>
2025-06-07 14:27:21 +0200

    Make GameWindow non-resizable and adjust size on pack

--- src/main/java/mainWindow/GameWindow.java
+++ src/main/java/mainWindow/GameWindow.java
@@ -4,11 +4,12 @@ import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
     public GameWindow(GamePanel gamePanel) {
-        this.setSize(400, 400);
         this.setTitle("Game Window");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(gamePanel);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.pack();
         this.setVisible(true);
     }
 }
