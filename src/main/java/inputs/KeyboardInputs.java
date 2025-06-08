6dd5ff209eb0e9906c1c085216dbc252312689c1
Kevin <pintokevin2002@hotmail.com>
2025-06-08 16:11:06 +0200

    Update KeyboardInputs to handle player movement directly through Player instance

--- src/main/java/inputs/KeyboardInputs.java
+++ src/main/java/inputs/KeyboardInputs.java
@@ -1,7 +1,6 @@
 package inputs;
 
 import mainWindow.GamePanel;
-import static utils.Constants.Directions.*;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -16,17 +15,20 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_W -> gamePanel.setDirection(UP);
-            case KeyEvent.VK_A -> gamePanel.setDirection(LEFT);
-            case KeyEvent.VK_S -> gamePanel.setDirection(DOWN);
-            case KeyEvent.VK_D -> gamePanel.setDirection(RIGHT);
+            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setUp(true);
+            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(true);
+            case KeyEvent.VK_S -> gamePanel.getGame().getPlayer().setDown(true);
+            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(true);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_W, KeyEvent.VK_A, KeyEvent.VK_S, KeyEvent.VK_D -> gamePanel.setMoving(false);
+            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setUp(false);
+            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(false);
+            case KeyEvent.VK_S -> gamePanel.getGame().getPlayer().setDown(false);
+            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(false);
         }
     }
 
