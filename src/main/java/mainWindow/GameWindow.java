bbd07eec46e2040c556b9687cbb18dc647675d04
Kevin <pintokevin2002@hotmail.com>
2025-06-08 16:17:25 +0200

    Add window focus listener to GameWindow for handling focus loss

--- src/main/java/mainWindow/GameWindow.java
+++ src/main/java/mainWindow/GameWindow.java
@@ -1,6 +1,8 @@
 package mainWindow;
 
 import javax.swing.JFrame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 public class GameWindow extends JFrame {
     public GameWindow(GamePanel gamePanel) {
@@ -11,5 +13,14 @@ public class GameWindow extends JFrame {
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
+        this.addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                gamePanel.getGame().windowFocusLost();
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {}
+        });
     }
 }
