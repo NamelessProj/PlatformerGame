cba575299fb55d4ca3906ad058fb9ca574dfb89d
Kevin <pintokevin2002@hotmail.com>
2025-06-09 18:13:24 +0200

    Update SCALE constant to improve rendering size

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -42,7 +42,7 @@ public class Constants {
         public static final int FPS_SET = 120;
         public static final int UPS_SET = 200;
         public static final int TILES_DEFAULT_SIZE = 32;
-        public static final float SCALE = 1.5f;
+        public static final float SCALE = 2f;
         public static final int TILES_IN_WIDTH = 26;
         public static final int TILES_IN_HEIGHT = 14;
         public static final int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
