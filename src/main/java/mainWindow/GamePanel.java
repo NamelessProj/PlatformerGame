606a90e26dd986dd296006afff36fd9836d2f0dc
Kevin <pintokevin2002@hotmail.com>
2025-06-09 11:16:19 +0200

    Update GamePanel to use game dimensions for panel size

--- src/main/java/mainWindow/GamePanel.java
+++ src/main/java/mainWindow/GamePanel.java
@@ -1,6 +1,7 @@
 package mainWindow;
 
-import static utils.Constants.GameConstants.*;
+import static utils.Constants.GameConstants.GAME_HEIGHT;
+import static utils.Constants.GameConstants.GAME_WIDTH;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
@@ -23,7 +24,8 @@ public class GamePanel extends JPanel {
     }
 
     private void setPanelSize() {
-        Dimension size = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT);
+        Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+        System.out.println("Setting panel size to: " + size);
         this.setMinimumSize(size);
         this.setPreferredSize(size);
         this.setMaximumSize(size);
