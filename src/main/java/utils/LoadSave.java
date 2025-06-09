3245edc780a778b9a8ddf6dcae8d4aca02b00c1b
Kevin <pintokevin2002@hotmail.com>
2025-06-09 11:07:07 +0200

    Add LoadSave utility class for loading sprite images

---
+++ src/main/java/utils/LoadSave.java
@@ -0,0 +1,27 @@
+package utils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadSave {
+    public static final String PLAYER_ATLAS = "images/player_sprites.png";
+
+    public static BufferedImage GetSpriteAtlas(String fileName) {
+        BufferedImage img = null;
+        InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return img;
+    }
+}
