f75d5f12e63fd8cb19bd622609c05b765f7593c8
Kevin <pintokevin2002@hotmail.com>
2025-06-09 11:26:48 +0200

    Add LevelManager class to handle level rendering

---
+++ src/main/java/levels/LevelManager.java
@@ -0,0 +1,23 @@
+package levels;
+
+import mainWindow.Game;
+import utils.LoadSave;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class LevelManager {
+    private Game game;
+    private BufferedImage levelSprite;
+
+    public LevelManager(Game game) {
+        this.game = game;
+        levelSprite = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(levelSprite, 0, 0, null);
+    }
+
+    public void update() {}
+}
