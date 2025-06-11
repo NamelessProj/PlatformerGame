12b05b6dbb3a79ee9aec28dc511a4e12371cbf93
Kevin <pintokevin2002@hotmail.com>
2025-06-11 14:37:17 +0200

    Enhance mouse input handling for MENU and PLAYING states

--- src/main/java/inputs/MouseInputs.java
+++ src/main/java/inputs/MouseInputs.java
@@ -17,19 +17,33 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         switch (Gamestate.state) {
-            case MENU -> gamePanel.getGame().getMenu().mouseClicked(e);
             case PLAYING -> gamePanel.getGame().getPlaying().mouseClicked(e);
         }
     }
 
     @Override
-    public void mouseMoved(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mouseMoved(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseMoved(e);
+        }
+    }
 
     @Override
-    public void mousePressed(MouseEvent e) {}
+    public void mousePressed(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mousePressed(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mousePressed(e);
+        }
+    }
 
     @Override
-    public void mouseReleased(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU -> gamePanel.getGame().getMenu().mouseReleased(e);
+            case PLAYING -> gamePanel.getGame().getPlaying().mouseReleased(e);
+        }
+    }
 
     @Override
     public void mouseEntered(MouseEvent e) {}
