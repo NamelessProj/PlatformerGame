4708d9157f906c3e26b63850266b6f7001bb2063
Kevin <pintokevin2002@hotmail.com>
2025-06-11 14:36:34 +0200

    Change hitbox dimensions from float to int for improved precision

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -32,7 +32,7 @@ public abstract class Entity {
      * @param width  The width of the hitbox.
      * @param height The height of the hitbox.
      */
-    protected void initHitbox(float x, float y, float width, float height) {
+    protected void initHitbox(float x, float y, int width, int height) {
         hitbox = new Rectangle2D.Float(x, y, width, height);
     }
 
