98eeb0fdb9c5f85946ec2b00971a04b7518cc994
Kevin <pintokevin2002@hotmail.com>
2025-06-11 14:37:34 +0200

    Add mouse event handling method to check button interaction

--- src/main/java/gamestates/State.java
+++ src/main/java/gamestates/State.java
@@ -1,12 +1,19 @@
 package gamestates;
 
 import mainWindow.Game;
+import ui.MenuButton;
+
+import java.awt.event.MouseEvent;
 
 public class State {
     protected Game game;
 
     public State(Game game) {
         this.game = game;
+    }
+
+    public boolean isIn(MouseEvent e, MenuButton btn) {
+        return btn.getBounds().contains(e.getX(), e.getY());
     }
 
     public Game getGame() {
