bde050d198644911226208c75da3a01db613b619
Kevin <pintokevin2002@hotmail.com>
2025-06-13 21:52:08 +0200

    Add EnemyManager class for managing Crabby enemies and their rendering

---
+++ src/main/java/entities/EnemyManager.java
@@ -0,0 +1,49 @@
+package entities;
+
+import gamestates.Playing;
+import utils.LoadSave;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import static utils.Constants.EnemyConstants.*;
+
+public class EnemyManager {
+    private Playing playing;
+    private BufferedImage[][] crabbyArr;
+    private ArrayList<Crabby> crabbies = new ArrayList<>();
+
+    public EnemyManager(Playing playing) {
+        this.playing = playing;
+        loadEnemyImages();
+        addEnemies();
+    }
+
+    private void addEnemies() {
+        crabbies = LoadSave.GetCrabs();
+        System.out.println("Number of crabbies: " + crabbies.size());
+    }
+
+    public void update() {
+        for (Crabby c : crabbies)
+            c.update();
+    }
+
+    public void draw(Graphics g, int xLvlOffset) {
+        drawCrabs(g, xLvlOffset);
+    }
+
+    private void drawCrabs(Graphics g, int xLvlOffset) {
+        for (Crabby c : crabbies)
+            g.drawImage(crabbyArr[c.getEnemyState()][c.getAnimationIndex()], (int) c.getHitbox().x - xLvlOffset, (int) c.getHitbox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+    }
+
+    private void loadEnemyImages() {
+        crabbyArr = new BufferedImage[5][9];
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
+        for (int j = 0; j < crabbyArr.length; j++)
+            for (int i = 0; i < crabbyArr[j].length; i++)
+                crabbyArr[j][i] = temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+    }
+}
