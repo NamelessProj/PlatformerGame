8572e47feec3fb510a9fc4ee23c36856a8d2616c
Kevin <pintokevin2002@hotmail.com>
2025-06-14 13:07:45 +0200

    Update drawHitbox method to account for level offset

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -39,9 +39,9 @@ public abstract class Entity {
     /**
      * For debugging purposes, draws the hitbox of the entity.
      */
-    protected void drawHitbox(Graphics g) {
+    protected void drawHitbox(Graphics g, int xLvlOffset) {
         g.setColor(Color.PINK);
-        g.drawRect((int) hitbox.x, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
+        g.drawRect((int) hitbox.x - xLvlOffset, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
     }
 
     /*protected void updateHitbox() {
