d6874828f818657a4af3d8309fa02edf6e48adcf
Kevin <pintokevin2002@hotmail.com>
2025-06-14 13:53:21 +0200

    Update EnemyManager to pass level data to Crabby updates and adjust drawing offsets

--- src/main/java/entities/EnemyManager.java
+++ src/main/java/entities/EnemyManager.java
@@ -25,9 +25,9 @@ public class EnemyManager {
         System.out.println("Number of crabbies: " + crabbies.size());
     }
 
-    public void update() {
+    public void update(int[][] lvlData) {
         for (Crabby c : crabbies)
-            c.update();
+            c.update(lvlData);
     }
 
     public void draw(Graphics g, int xLvlOffset) {
@@ -36,7 +36,7 @@ public class EnemyManager {
 
     private void drawCrabs(Graphics g, int xLvlOffset) {
         for (Crabby c : crabbies)
-            g.drawImage(crabbyArr[c.getEnemyState()][c.getAnimationIndex()], (int) c.getHitbox().x - xLvlOffset, (int) c.getHitbox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+            g.drawImage(crabbyArr[c.getEnemyState()][c.getAnimationIndex()], (int) (c.getHitbox().x - CRABBY_DRAWOFFSET_X) - xLvlOffset, (int) (c.getHitbox().y - CRABBY_DRAWOFFSET_Y), CRABBY_WIDTH, CRABBY_HEIGHT, null);
     }
 
     private void loadEnemyImages() {
