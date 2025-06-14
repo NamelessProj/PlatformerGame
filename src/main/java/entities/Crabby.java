1c09a2b4cc855de4296b39b3a56cc9a673235321
Kevin <pintokevin2002@hotmail.com>
2025-06-14 13:53:13 +0200

    Initialize hitbox dimensions for Crabby using scale factor

--- src/main/java/entities/Crabby.java
+++ src/main/java/entities/Crabby.java
@@ -1,9 +1,11 @@
 package entities;
 
 import static utils.Constants.EnemyConstants.*;
+import static utils.Constants.GameConstants.SCALE;
 
 public class Crabby extends Enemy {
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+        initHitbox(x, y, (int) (22 * SCALE), (int) (19 * SCALE));
     }
 }
