6aeddcaf2363520d7b621df97f820bb4aaee456d
Kevin <pintokevin2002@hotmail.com>
2025-06-14 18:38:18 +0200

    Add attack box logic to Crabby class for improved collision detection

--- src/main/java/entities/Crabby.java
+++ src/main/java/entities/Crabby.java
@@ -1,18 +1,36 @@
 package entities;
 
+import java.awt.geom.Rectangle2D;
+
 import static utils.Constants.Directions.*;
 import static utils.Constants.EnemyConstants.*;
 import static utils.Constants.GameConstants.SCALE;
 
 public class Crabby extends Enemy {
+    // AttackBox
+    private Rectangle2D.Float attackBox;
+    private int attackBoxOffsetX;
+
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
         initHitbox(x, y, (int) (22 * SCALE), (int) (19 * SCALE));
+        initAttackBox();
+    }
+
+    private void initAttackBox() {
+        attackBox = new Rectangle2D.Float(x, y, (int) (82 * SCALE), (int) (19 * SCALE));
+        attackBoxOffsetX = (int) (30 * SCALE);
     }
 
     protected void update(int[][] lvlData, Player player) {
         updateMove(lvlData, player);
         updateAnimationTick();
+        updateAttackBox();
+    }
+
+    private void updateAttackBox() {
+        attackBox.x = hitbox.x - attackBoxOffsetX;
+        attackBox.y = hitbox.y;
     }
 
     private void updateMove(int[][] lvlData, Player player) {
