29af993bb04c999661c352c5896b7252d6bb4c4b
Kevin <pintokevin2002@hotmail.com>
2025-06-14 14:22:06 +0200

    Implement movement and update logic for Crabby enemy

--- src/main/java/entities/Crabby.java
+++ src/main/java/entities/Crabby.java
@@ -1,11 +1,32 @@
 package entities;
 
+import static utils.Constants.Directions.LEFT;
 import static utils.Constants.EnemyConstants.*;
 import static utils.Constants.GameConstants.SCALE;
+import static utils.HelpMethods.*;
 
 public class Crabby extends Enemy {
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
         initHitbox(x, y, (int) (22 * SCALE), (int) (19 * SCALE));
+    }
+
+    protected void update(int[][] lvlData) {
+        updateMove(lvlData);
+        updateAnimationTick();
+    }
+
+    private void updateMove(int[][] lvlData) {
+        if (firstUpdate)
+            firstUpdateCheck(lvlData);
+
+        if (inAir)
+            updateInAir(lvlData);
+        else {
+            switch (enemyState) {
+                case IDLE -> newState(RUNNING);
+                case RUNNING -> move(lvlData);
+            }
+        }
     }
 }
