870cfd61453dbfdb177224cb3855c664207b5794
Kevin <pintokevin2002@hotmail.com>
2025-06-15 23:45:43 +0200

    Enhance Level class to support enemy creation and player spawn point retrieval from images

--- src/main/java/levels/Level.java
+++ src/main/java/levels/Level.java
@@ -1,10 +1,47 @@
 package levels;
 
-public class Level {
-    private int[][] lvlData;
+import entities.Crabby;
 
-    public Level(int[][] lvlData) {
-        this.lvlData = lvlData;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import static utils.Constants.GameConstants.*;
+import static utils.HelpMethods.*;
+
+public class Level {
+    private BufferedImage image;
+    private int[][] lvlData;
+    private ArrayList<Crabby> crabs;
+    private int levelTilesWide;
+    private int maxTilesOffset;
+    private int maxLevelOffsetX;
+    private Point playerSpawn;
+
+    public Level(BufferedImage image) {
+        this.image = image;
+        createLevelData();
+        createEnemies();
+        calculateLevelOffsets();
+        calculatePlayerSpawn();
+    }
+
+    private void calculatePlayerSpawn() {
+        playerSpawn = GetPlayerSpawn(image);
+    }
+
+    private void calculateLevelOffsets() {
+        levelTilesWide = image.getWidth();
+        maxTilesOffset = levelTilesWide - TILES_IN_WIDTH;
+        maxLevelOffsetX = TILES_SIZE * maxTilesOffset;
+    }
+
+    private void createEnemies() {
+        crabs = GetCrabs(image);
+    }
+
+    private void createLevelData() {
+        lvlData = GetLevelData(image);
     }
 
     public int getSpriteIndex(int x, int y) {
@@ -13,5 +50,17 @@ public class Level {
 
     public int[][] getLevelData() {
         return lvlData;
+    }
+
+    public int getLevelOffsetX() {
+        return maxLevelOffsetX;
+    }
+
+    public ArrayList<Crabby> getCrabs() {
+        return crabs;
+    }
+
+    public Point getPlayerSpawn() {
+        return playerSpawn;
     }
 }
