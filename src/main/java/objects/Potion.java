4c56ac194c6104fe672f79760c1f75e8c4df49e3
Kevin <pintokevin2002@hotmail.com>
2025-06-16 22:09:13 +0200

    Add hovering effect to Potion class with dynamic hitbox adjustment

--- src/main/java/objects/Potion.java
+++ src/main/java/objects/Potion.java
@@ -3,15 +3,37 @@ package objects;
 import static utils.Constants.GameConstants.SCALE;
 
 public class Potion extends GameObject {
+    private static class Direction {
+        public static int DOWN = 1;
+        public static int UP = -1;
+    }
+
+    private float hoverOffset, hoverSpeed = 0.075f * SCALE;
+    private int maxHoverOffset, hoverDirection = Direction.DOWN;
+
     public Potion(int x, int y, int objectType) {
         super(x, y, objectType);
         this.doAnimation = true;
         this.initHitbox(7, 14);
         this.xDrawOffset = (int) (3 * SCALE);
         this.yDrawOffset = (int) (2 * SCALE);
+
+        maxHoverOffset = (int) (10 * SCALE);
     }
 
     public void update() {
         updateAnimationTick();
+        updateHover();
+    }
+
+    private void updateHover() {
+        hoverOffset += hoverSpeed * hoverDirection;
+
+        if (hoverOffset >= maxHoverOffset)
+            hoverDirection = Direction.UP;
+        else if (hoverOffset < 0)
+            hoverDirection = Direction.DOWN;
+
+        hitbox.y = y + hoverOffset;
     }
 }
