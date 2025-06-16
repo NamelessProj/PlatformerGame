863b7a72e64f235fbfc2120afdf672a9217449ba
Kevin <pintokevin2002@hotmail.com>
2025-06-16 14:59:00 +0200

    Add GameContainer class to represent game containers with hitbox functionality and animation support

---
+++ src/main/java/objects/GameContainer.java
@@ -0,0 +1,28 @@
+package objects;
+
+import static utils.Constants.GameConstants.SCALE;
+import static utils.Constants.ObjectConstants.*;
+
+public class GameContainer  extends GameObject{
+    public GameContainer(int x, int y, int objectType) {
+        super(x, y, objectType);
+        createHitbox();
+    }
+
+    private void createHitbox() {
+        if (objectType == BOX) {
+            initHitbox(25, 18);
+            xDrawOffset = (int) (7 * SCALE);
+            yDrawOffset = (int) (12 * SCALE);
+        } else {
+            initHitbox(23, 25);
+            xDrawOffset = (int) (8 * SCALE);
+            yDrawOffset = (int) (5 * SCALE);
+        }
+    }
+
+    public void update() {
+        if (doAnimation)
+            updateAnimationTick();
+    }
+}
