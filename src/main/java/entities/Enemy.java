23d3b4f477524cc4e20979b959db410a055a03e3
Kevin <pintokevin2002@hotmail.com>
2025-06-16 13:27:35 +0200

    Remove redundant hitbox initialization in Enemy constructor

--- src/main/java/entities/Enemy.java
+++ src/main/java/entities/Enemy.java
@@ -19,7 +19,6 @@ public abstract class Enemy extends Entity {
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
         this.enemyType = enemyType;
-        initHitbox(x, y, width, height);
         maxHealth = GetMaxHealth(enemyType);
         currentHealth = maxHealth;
         walkSpeed = SCALE * 0.35f;
