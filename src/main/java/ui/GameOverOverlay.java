e33d33fb9e35fb423b37a2d23eba42720402097e
Kevin <pintokevin2002@hotmail.com>
2025-06-18 21:38:51 +0200

    Add menu and play buttons to game over overlay with mouse interaction handling

--- src/main/java/ui/GameOverOverlay.java
+++ src/main/java/ui/GameOverOverlay.java
@@ -6,18 +6,30 @@ import utils.LoadSave;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import static utils.Constants.GameConstants.*;
+import static utils.Constants.UI.URMButtons.URM_SIZE;
 
 public class GameOverOverlay {
     private Playing playing;
     private BufferedImage img;
     private int imgX, imgY, imgW, imgH;
+    private UrmButton menu, play;
 
     public GameOverOverlay(Playing playing) {
         this.playing = playing;
         createImg();
+        createButtons();
+    }
+
+    private void createButtons() {
+        int menuX = (int) (335 * SCALE);
+        int playX = (int) (440 * SCALE);
+        int y = (int) (195 * SCALE);
+        play = new UrmButton(playX, y, URM_SIZE, URM_SIZE, 0);
+        menu = new UrmButton(menuX, y, URM_SIZE, URM_SIZE, 2);
     }
 
     private void createImg() {
@@ -33,6 +45,14 @@ public class GameOverOverlay {
         g.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
         g.drawImage(img, imgX, imgY, imgW, imgH, null);
+
+        menu.draw(g);
+        play.draw(g);
+    }
+
+    public void update() {
+        menu.update();
+        play.update();
     }
 
     public void keyPressed(KeyEvent e) {
@@ -40,5 +60,38 @@ public class GameOverOverlay {
             playing.resetAll();
             Gamestate.state = Gamestate.MENU;
         }
+    }
+
+    private boolean isIn(UrmButton b, MouseEvent e) {
+        return b.getBounds().contains(e.getX(), e.getY());
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        play.setMouseOver(false);
+        menu.setMouseOver(false);
+
+        if (isIn(menu, e))
+            menu.setMouseOver(true);
+        else if (isIn(play, e))
+            play.setMouseOver(true);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        if (isIn(menu, e) && menu.isMousePressed()) {
+            playing.resetAll();
+            Gamestate.state = Gamestate.MENU;
+        } else if (isIn(play, e) && play.isMousePressed()) {
+            //playing.loadNextLevel();
+        }
+
+        play.resetBools();
+        menu.resetBools();
+    }
+
+    public void mousePressed(MouseEvent e) {
+        if (isIn(menu, e))
+            menu.setMousePressed(true);
+        else if (isIn(play, e))
+            play.setMousePressed(true);
     }
 }
