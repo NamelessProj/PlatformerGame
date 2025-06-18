6a9dcaa8d25253e48e6e47e108f92fef3af2d051
Kevin <pintokevin2002@hotmail.com>
2025-06-18 21:23:22 +0200

    Display death screen image in game over overlay

--- src/main/java/ui/GameOverOverlay.java
+++ src/main/java/ui/GameOverOverlay.java
@@ -2,26 +2,37 @@ package ui;
 
 import gamestates.Gamestate;
 import gamestates.Playing;
+import utils.LoadSave;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import static utils.Constants.GameConstants.*;
 
 public class GameOverOverlay {
     private Playing playing;
+    private BufferedImage img;
+    private int imgX, imgY, imgW, imgH;
 
     public GameOverOverlay(Playing playing) {
         this.playing = playing;
+        createImg();
+    }
+
+    private void createImg() {
+        img = LoadSave.GetSpriteAtlas(LoadSave.DEATH_SCREEN);
+        imgW = (int) (img.getWidth() * SCALE);
+        imgH = (int) (img.getHeight() * SCALE);
+        imgX = GAME_WIDTH / 2 - imgW / 2;
+        imgY = (int) (100 * SCALE);
     }
 
     public void draw(Graphics g) {
         g.setColor(new Color(0, 0, 0, 200));
         g.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
-        g.setColor(Color.WHITE);
-        g.drawString("Game Over", GAME_WIDTH / 2, 150);
-        g.drawString("Press esc to enter Main Menu!", GAME_WIDTH / 2, 300);
+        g.drawImage(img, imgX, imgY, imgW, imgH, null);
     }
 
     public void keyPressed(KeyEvent e) {
