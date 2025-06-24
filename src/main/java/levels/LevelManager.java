12cd98a0e1aee98c83f3374ebd2704c2695dd001
Kevin <pintokevin2002@hotmail.com>
2025-06-24 15:02:08 +0200

    Add water sprite handling and animation to LevelManager

--- src/main/java/levels/LevelManager.java
+++ src/main/java/levels/LevelManager.java
@@ -12,15 +12,25 @@ import static utils.Constants.GameConstants.*;
 
 public class LevelManager {
     private Game game;
-    private BufferedImage[] levelSprite;
+    private BufferedImage[] levelSprite, waterSprite;
     private ArrayList<Level> levels;
-    private int levelIndex = 0;
+    private int levelIndex = 0, animationTick, animationIndex, waterBottomIndex;
 
     public LevelManager(Game game) {
         this.game = game;
         importOutsideSprites();
+        createWater();
         levels = new ArrayList<>();
         builtAllLevels();
+    }
+
+    private void createWater() {
+        waterSprite = new BufferedImage[5];
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.WATER_TOP);
+        waterBottomIndex = waterSprite.length - 1;
+        for (int i = 0; i < waterBottomIndex; i++)
+            waterSprite[i] = temp.getSubimage(i * TILES_DEFAULT_SIZE, 0, TILES_DEFAULT_SIZE, TILES_DEFAULT_SIZE);
+        waterSprite[waterBottomIndex] = LoadSave.GetSpriteAtlas(LoadSave.WATER_BOTTOM);
     }
 
     private void builtAllLevels() {
@@ -47,11 +57,31 @@ public class LevelManager {
         for (int j = 0; j < TILES_IN_HEIGHT; j++)
             for (int i = 0; i < levels.get(levelIndex).getLevelData()[0].length; i++) {
                 int index = levels.get(levelIndex).getSpriteIndex(i, j);
-                g.drawImage(levelSprite[index], TILES_SIZE * i - xLvlOffset, TILES_SIZE * j, TILES_SIZE, TILES_SIZE, null);
+                int x = TILES_SIZE * i - xLvlOffset;
+                int y = TILES_SIZE * j;
+                if (index == 48)
+                    g.drawImage(levelSprite[animationIndex], x, y, TILES_SIZE, TILES_SIZE, null);
+                else if (index == 49)
+                    g.drawImage(waterSprite[waterBottomIndex], x, y, TILES_SIZE, TILES_SIZE, null);
+                else
+                    g.drawImage(levelSprite[index], x, y, TILES_SIZE, TILES_SIZE, null);
             }
     }
 
-    public void update() {}
+    public void update() {
+        updateWaterAnimation();
+    }
+
+    private void updateWaterAnimation() {
+        animationTick++;
+        if (animationTick >= 40) {
+            animationTick = 0;
+            animationIndex++;
+            
+            if (animationIndex >= waterBottomIndex)
+                animationIndex = 0;
+        }
+    }
 
     public Level getCurrentLevel() {
         return levels.get(levelIndex);
