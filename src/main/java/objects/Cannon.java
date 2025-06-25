bd76afd37e07336e9e0f5e615db6b854261943f9
Kevin <pintokevin2002@hotmail.com>
2025-06-25 16:37:26 +0200

    Add detailed method documentation to Cannon class for improved clarity and maintainability

--- src/main/java/objects/Cannon.java
+++ src/main/java/objects/Cannon.java
@@ -6,6 +6,12 @@ import static utils.Constants.GameConstants.TILES_SIZE;
 public class Cannon extends GameObject {
     private int tileY;
     
+    /**
+     * Constructor for the Cannon class.
+     * @param x the x-coordinate of the cannon
+     * @param y the y-coordinate of the cannon
+     * @param objectType the type of the object (CANNON)
+     */
     public Cannon(int x, int y, int objectType) {
         super(x, y, objectType);
         tileY = y / TILES_SIZE;
@@ -14,11 +20,18 @@ public class Cannon extends GameObject {
         this.hitbox.y += (int) (6 * SCALE);
     }
 
+    /**
+     * Updates the animation tick for the cannon.
+     */
     public void update() {
         if (doAnimation)
             updateAnimationTick();
     }
 
+    /**
+     * Returns the tile Y coordinate of the cannon.
+     * @return the tile Y coordinate
+     */
     public int getTileY() {
         return tileY;
     }
