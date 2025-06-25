c3c052a0f4f04950ca68e61879aed2ec1c765ff0
Kevin <pintokevin2002@hotmail.com>
2025-06-25 16:37:41 +0200

    Add detailed method documentation to Grass class for improved clarity and maintainability

--- src/main/java/objects/Grass.java
+++ src/main/java/objects/Grass.java
@@ -3,20 +3,38 @@ package objects;
 public class Grass {
     private int x, y, type;
 
+    /**
+     * Constructor for the Grass class.
+     * @param x the x-coordinate of the grass
+     * @param y the y-coordinate of the grass
+     * @param type the type of the grass
+     */
     public Grass(int x, int y, int type) {
         this.x = x;
         this.y = y;
         this.type = type;
     }
 
+    /**
+     * Returns the x-coordinate of the grass.
+     * @return the x-coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Returns the y-coordinate of the grass.
+     * @return the y-coordinate
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Returns the type of the grass.
+     * @return the type of the grass
+     */
     public int getType() {
         return type;
     }
