539f1ef90a8d68f42593262be1b3c91fc3ac29b9
Kevin <pintokevin2002@hotmail.com>
2025-06-25 17:05:41 +0200

    Add detailed method documentation to VolumeButton class for improved clarity and maintainability

--- src/main/java/ui/VolumeButton.java
+++ src/main/java/ui/VolumeButton.java
@@ -15,6 +15,13 @@ public class VolumeButton extends PauseButton {
     private int buttonX, minX, maxX;
     private float floatValue = 0;
 
+    /**
+     * Constructor for the VolumeButton class.
+     * @param x the x-coordinate of the button
+     * @param y the y-coordinate of the button
+     * @param width the width of the button
+     * @param height the height of the button
+     */
     public VolumeButton(int x, int y, int width, int height) {
         super(x + width / 2, y, VOLUME_WIDTH, height);
         bounds.x -= VOLUME_WIDTH / 2;
@@ -26,6 +33,9 @@ public class VolumeButton extends PauseButton {
         loadImages();
     }
 
+    /**
+     * Loads the button images from the sprite sheet.
+     */
     private void loadImages() {
         BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.VOLUME_BUTTONS);
         imgs = new BufferedImage[3];
@@ -35,6 +45,9 @@ public class VolumeButton extends PauseButton {
         sliderImg = temp.getSubimage(3 * VOLUME_DEFAULT_WIDTH, 0, SLIDER_DEFAULT_WIDTH, VOLUME_DEFAULT_HEIGHT);
     }
 
+    /**
+     * Updates the state of the volume button based on mouse interaction.
+     */
     public void update() {
         index = 0;
         if (mouseOver)
@@ -43,11 +56,19 @@ public class VolumeButton extends PauseButton {
             index = 2;
     }
 
+    /**
+     * Draws the volume button on the screen.
+     * @param g the Graphics object used for drawing
+     */
     public void draw(Graphics g) {
         g.drawImage(sliderImg, x, y, width, height, null);
         g.drawImage(imgs[index], buttonX - VOLUME_WIDTH / 2, y, VOLUME_WIDTH, height, null);
     }
 
+    /**
+     * Changes the x-coordinate of the button based on the provided value.
+     * @param x the new x-coordinate for the button
+     */
     public void changeX(int x) {
         if (x < minX)
             buttonX = minX;
@@ -60,33 +81,59 @@ public class VolumeButton extends PauseButton {
         bounds.x = buttonX - VOLUME_WIDTH / 2;
     }
 
+    /**
+     * Updates the float value based on the button's position.
+     */
     private void updateFloatValue() {
         float range = maxX - minX;
         float val = buttonX - minX;
         floatValue = val / range;
     }
 
+    /**
+     * Resets the mouse interaction states of the button.
+     */
     public void resetBools() {
         mouseOver = false;
         mousePressed = false;
     }
 
+    /**
+     * Checks if the mouse is currently over the button.
+     * @return true if the mouse is over the button, false otherwise
+     */
     public boolean isMouseOver() {
         return mouseOver;
     }
 
+    /**
+     * Sets the mouse over state of the button.
+     * @param mouseOver true to set the button as hovered, false otherwise
+     */
     public void setMouseOver(boolean mouseOver) {
         this.mouseOver = mouseOver;
     }
 
+    /**
+     * Checks if the mouse is currently pressed on the button.
+     * @return true if the mouse is pressed, false otherwise
+     */
     public boolean isMousePressed() {
         return mousePressed;
     }
 
+    /**
+     * Sets the mouse pressed state of the button.
+     * @param mousePressed true to set the button as pressed, false otherwise
+     */
     public void setMousePressed(boolean mousePressed) {
         this.mousePressed = mousePressed;
     }
 
+    /**
+     * Returns the current float value representing the volume level.
+     * @return the float value of the volume level
+     */
     public float getFloatValue() {
         return floatValue;
     }
