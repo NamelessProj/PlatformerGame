cea0ef2d84c3ac9f1d057f28b984378e6c22a1d4
Kevin <pintokevin2002@hotmail.com>
2025-06-25 15:39:08 +0200

    Add detailed method documentation to Crabby class for improved clarity and maintainability

--- src/main/java/entities/Crabby.java
+++ src/main/java/entities/Crabby.java
@@ -7,23 +7,42 @@ import static utils.Constants.EnemyConstants.*;
 import static utils.Constants.GameConstants.SCALE;
 
 public class Crabby extends Enemy {
+
+    /**
+     * Crabby enemy constructor.
+     * @param x The x-coordinate of the Crabby's position.
+     * @param y The y-coordinate of the Crabby's position.
+    */
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
         initHitbox(22, 19);
         initAttackBox();
     }
 
+    /**
+     * Initializes the hitbox for the Crabby enemy.
+     */
     private void initAttackBox() {
         attackBox = new Rectangle2D.Float(x, y, (int) (82 * SCALE), (int) (19 * SCALE));
         attackBoxOffsetX = (int) (30 * SCALE);
     }
 
+    /**
+     * Updates the Crabby's state and behavior based on the level data and player position.
+     * @param lvlData The level data containing information about the environment.
+     * @param player The player entity that the Crabby interacts with.
+     */
     protected void update(int[][] lvlData, Player player) {
         updateBehavior(lvlData, player);
         updateAnimationTick();
         updateAttackBox();
     }
 
+    /**
+     * Updates the Crabby's behavior based on its current state and the player's position.
+     * @param lvlData The level data containing information about the environment.
+     * @param player The player entity that the Crabby interacts with.
+     */
     private void updateBehavior(int[][] lvlData, Player player) {
         if (firstUpdate)
             firstUpdateCheck(lvlData);
@@ -54,6 +73,10 @@ public class Crabby extends Enemy {
         }
     }
 
+    /**
+     * Flips the Crabby's hitbox based on its walking direction.
+     * @return The x-coordinate offset for flipping the hitbox.
+     */
     public int flipX() {
         if (walkDir == RIGHT)
             return width;
@@ -61,6 +84,10 @@ public class Crabby extends Enemy {
             return 0;
     }
 
+    /**
+     * Flips the Crabby's walking direction.
+     * @return The direction multiplier for flipping the walking direction.
+     */
     public int flipW() {
         if (walkDir == RIGHT)
             return -1;
