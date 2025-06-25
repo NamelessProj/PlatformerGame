a656b4bf0dd1c5cfef112095c8b78a040e56ab2f
Kevin <pintokevin2002@hotmail.com>
2025-06-25 16:38:04 +0200

    Add detailed method documentation to Spike class constructor for improved clarity and maintainability

--- src/main/java/objects/Spike.java
+++ src/main/java/objects/Spike.java
@@ -3,6 +3,12 @@ package objects;
 import static utils.Constants.GameConstants.SCALE;
 
 public class Spike extends GameObject {
+    /**
+     * Constructor for the Spike class.
+     * @param x the x-coordinate of the spike
+     * @param y the y-coordinate of the spike
+     * @param objectType the type of the object (SPIKE)
+     */
     public Spike(int x, int y, int objectType) {
         super(x, y, objectType);
         initHitbox(32, 16);
