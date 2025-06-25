27a0bb9b93c05cb219347d367bbc01b5b488ccfc
Kevin <pintokevin2002@hotmail.com>
2025-06-25 15:56:25 +0200

    Add detailed documentation for the current game state in Gamestate enum

--- src/main/java/gamestates/Gamestate.java
+++ src/main/java/gamestates/Gamestate.java
@@ -3,5 +3,8 @@ package gamestates;
 public enum Gamestate {
     PLAYING, MENU, OPTIONS, QUIT;
 
+    /**
+     * The current state of the game.
+     */
     public static Gamestate state = MENU;
 }
