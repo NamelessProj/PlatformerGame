2dd7e16b905e1e39996d25ca858b02fe8a9a6cd4
Kevin <pintokevin2002@hotmail.com>
2025-07-01 12:28:35 +0200

    Add missing ICON constant to LoadSave class

--- src/main/java/utils/LoadSave.java
+++ src/main/java/utils/LoadSave.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class LoadSave {
+    public static final String ICON = "icon.png";
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
