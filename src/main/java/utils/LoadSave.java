ef186294b4fd3ab5cd003de0d86411e35e1d5a9c
Kevin <pintokevin2002@hotmail.com>
2025-07-02 13:29:10 +0200

    Reorganize background image constants in LoadSave class

--- src/main/java/utils/LoadSave.java
+++ src/main/java/utils/LoadSave.java
@@ -13,19 +13,21 @@ public class LoadSave {
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
-    public static final String MENU_BACKGROUND = "menu_background.png";
-    public static final String PAUSE_BACKGROUND = "pause_menu.png";
     public static final String SOUND_BUTTONS = "sound_button.png";
     public static final String URM_BUTTONS = "urm_buttons.png";
     public static final String VOLUME_BUTTONS = "volume_buttons.png";
-    public static final String MENU_BACKGROUND_IMAGE = "background_menu.png";
     public static final String PLAYING_BACKGROUND_IMAGE = "playing_bg_img.png";
     public static final String BIG_CLOUDS = "big_clouds.png";
     public static final String SMALL_CLOUDS = "small_clouds.png";
     public static final String STATUS_BAR = "health_power_bar.png";
+
+    public static final String MENU_BACKGROUND = "menu_background.png";
+    public static final String PAUSE_BACKGROUND = "pause_menu.png";
+    public static final String MENU_BACKGROUND_IMAGE = "background_menu.png";
     public static final String COMPLETED_IMG = "completed_sprite.png";
     public static final String DEATH_SCREEN = "death_screen.png";
     public static final String OPTIONS_MENU = "options_background.png";
+    public static final String GAME_COMPLETED = "game_completed.png";
 
     public static final String QUESTION_ATLAS = "question_atlas.png";
 	public static final String EXCLAMATION_ATLAS = "exclamation_atlas.png";
