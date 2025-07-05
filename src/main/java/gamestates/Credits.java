39a28d08fc91a343c985974ada3e52824df08c84
Kevin <pintokevin2002@hotmail.com>
2025-07-05 14:27:44 +0200

    Add Credits class for managing the credits screen functionality

---
+++ src/main/java/gamestates/Credits.java
@@ -0,0 +1,53 @@
+package gamestates;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import mainWindow.Game;
+
+public class Credits extends State implements Statemethods {
+    private BufferedImage backgroundImg;
+    private int bgX, bgY, bgW, bgH;
+    private float bgYFloat;
+
+    public Credits(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            bgYFloat = 0;
+            setGamestate(Gamestate.MENU);
+        }
+    }
+}
