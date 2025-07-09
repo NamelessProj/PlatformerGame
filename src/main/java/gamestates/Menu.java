87c592ac6e15be57f5a712dafaac9393e904434a
Kevin <pintokevin2002@hotmail.com>
2025-07-09 14:52:44 +0200

    Adjust button positions in menu for improved layout

--- src/main/java/gamestates/Menu.java
+++ src/main/java/gamestates/Menu.java
@@ -43,10 +43,10 @@ public class Menu extends State implements Statemethods {
      * Loads the buttons for the menu.
      */
     private void loadButtons() {
-        buttons[0] = new MenuButton(GAME_WIDTH / 2, (int) (130 * SCALE), 0, Gamestate.PLAYING);
-        buttons[1] = new MenuButton(GAME_WIDTH / 2, (int) (200 * SCALE), 1, Gamestate.OPTIONS);
-        buttons[2] = new MenuButton(GAME_WIDTH / 2, (int) (270 * SCALE), 3, Gamestate.CREDITS);
-        buttons[3] = new MenuButton(GAME_WIDTH / 2, (int) (340 * SCALE), 2, Gamestate.QUIT);
+        buttons[0] = new MenuButton(GAME_WIDTH / 2, (int) (150 * SCALE), 0, Gamestate.PLAYING);
+        buttons[1] = new MenuButton(GAME_WIDTH / 2, (int) (220 * SCALE), 1, Gamestate.OPTIONS);
+        buttons[2] = new MenuButton(GAME_WIDTH / 2, (int) (290 * SCALE), 3, Gamestate.CREDITS);
+        buttons[3] = new MenuButton(GAME_WIDTH / 2, (int) (360 * SCALE), 2, Gamestate.QUIT);
     }
 
     /**
