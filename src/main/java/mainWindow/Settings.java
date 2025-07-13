c45cfff8619c1bf97eaa258346d43819d67e7d4f
Kevin <pintokevin2002@hotmail.com>
2025-07-13 15:45:54 +0200

    Implement settings loading with volume validation and defaulting

---
+++ src/main/java/mainWindow/Settings.java
@@ -0,0 +1,75 @@
+package mainWindow;
+
+import java.io.BufferedReader;
+
+import utils.LoadSave;
+
+import static utils.HelpMethods.IsFloat;
+
+public class Settings {
+    public class Const {
+        public static final int NUM_SETTINGS = 3;
+        public static final String SOUND_MUTED = "sound_muted";
+        public static final String MUSIC_MUTED = "music_muted";
+        public static final String VOLUME = "volume";
+    }
+
+    private Game game;
+
+    public Settings(Game game) {
+        this.game = game;
+        loadSettings();
+    }
+
+    private void loadSettings() {
+        BufferedReader reader = LoadSave.GetText(LoadSave.Texts.SETTINGS);
+
+        if (reader != null) {
+            try {
+                int i = 0;
+                String line = reader.readLine();
+                while (line != null) {
+                    if (line.isBlank() || !line.contains("="))
+                        continue;
+
+                    if (++i >= Const.NUM_SETTINGS)
+                        break;
+
+                    String[] parts = line.toLowerCase().split("=", 2);
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+
+                    switch (key) {
+                        case Const.MUSIC_MUTED -> {
+                            boolean muted = value.equals("true");
+                            game.getAudioOptions().setMusicMuted(muted);
+                        }
+                        case Const.SOUND_MUTED -> {
+                            boolean muted = value.equals("true");
+                            game.getAudioOptions().setSoundMuted(muted);
+                        }
+                        case Const.VOLUME -> {
+                            if (IsFloat(value)) {
+                                float volume = Float.parseFloat(value);
+                                if (volume < 0 || volume > 1)
+                                    volume = 0.5f;
+                                game.getAudioOptions().setVolume(volume);
+                            }
+                        }
+                    }
+
+                    line = reader.readLine();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            createSettingsFile();
+        }
+    }
+
+    private void createSettingsFile() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createSettingsFile'");
+    }
+}
