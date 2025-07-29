adf7c5e850293260b901cc3a902db226ffea2f5c
Kevin <pintokevin2002@hotmail.com>
2025-07-29 12:29:21 +0200

    Enhance documentation for BackgroundTree class methods and constructor

--- src/main/java/objects/BackgroundTree.java
+++ src/main/java/objects/BackgroundTree.java
@@ -7,15 +7,25 @@ import java.util.Random;
 public class BackgroundTree {
     private int x, y, type, animationIndex, animationTick;
 
+    /**
+     * Constructor for the BackgroundTree class.
+     * @param x The x-coordinate of the tree.
+     * @param y The y-coordinate of the tree.
+     * @param type The type of the tree (e.g., {@link utils.Constants.Tree#TREE_ONE}, {@link utils.Constants.Tree#TREE_TWO}, etc.).
+     */
     public BackgroundTree(int x, int y, int type) {
         this.x = x;
         this.y = y;
         this.type = type;
         
+        // Randomly select an animation index for the tree so that each tree can have a different initial animation.
         Random r = new Random();
         animationIndex = r.nextInt(NUM_ANIMATIONS);
     }
 
+    /**
+     * Updates the animation index of the tree.
+     */
     public void update() {
         animationTick++;
         if (animationTick >= 35) {
@@ -26,30 +36,58 @@ public class BackgroundTree {
         }
     }
 
+    /**
+     * Gets the x-coordinate of the tree.
+     * @return The x-coordinate of the tree.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Gets the y-coordinate of the tree.
+     * @return The y-coordinate of the tree.
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Gets the type of the tree.
+     * @return The type of the tree (e.g., {@link utils.Constants.Tree#TREE_ONE}, {@link utils.Constants.Tree#TREE_TWO}, etc.).
+     */
     public int getType() {
         return type;
     }
 
+    /**
+     * Gets the current animation index of the tree.
+     * @return The current animation index.
+     */
     public int getAnimationIndex() {
         return animationIndex;
     }
 
+    /**
+     * Sets the x-coordinate of the tree.
+     * @param x The new x-coordinate.
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Sets the y-coordinate of the tree.
+     * @param y The new y-coordinate.
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * Sets the type of the tree.
+     * @param type The new type of the tree (e.g., {@link utils.Constants.Tree#TREE_ONE}, {@link utils.Constants.Tree#TREE_TWO}, etc.).
+     */
     public void setType(int type) {
         this.type = type;
     }
